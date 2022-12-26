9c6c4149be9d0f6178375ee4cd6ffa7eedf98211
Dhiraj Kumar Mehta <dk266107@gmail.com>
2022-12-26 11:03:48 +0530

    Project added

---
+++ Oerder-serivice/src/main/java/com/dmehta/os/api/OerderSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.dmehta.os.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OerderSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OerderSeriviceApplication.class, args);
+	}
+
+}
---
+++ Oerder-serivice/src/test/java/com/dmehta/os/api/OerderSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmehta.os.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OerderSeriviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
